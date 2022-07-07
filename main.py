38d32810700ffb8cd0dca17c25b2443a5348fe43
Reckonzz <57397118+Reckonzz@users.noreply.github.com>
2022-07-07 21:42:38 +0800

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model import clustering_model
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route('/')
 def index():
